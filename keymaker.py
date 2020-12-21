37ec5c3891cdcb4cd7bd1a19d16dee7c9b72ba6d
Aldan Dániel <d.aldan12.ad@gmail.com>
2020-12-21 18:49:05 +0100

    Hash it function added

--- keymaker.py
+++ keymaker.py
@@ -21,11 +21,7 @@ def shift_characters(word, shift):
     return solution
 
 
-print(shift_characters("zozo", 5))
-
-
 def pad_up_to(word, shift, n):
-    word=word.lower()
     solution=word
     while len(solution) <= n:
         letter = [char for char in word]
@@ -45,8 +41,6 @@ def pad_up_to(word, shift, n):
         word=saver
         saver=""
     return solution[0:n]
-
-print(pad_up_to("abb", 5, 11))
 
 
 def abc_mirror(word):
@@ -70,52 +64,60 @@ def abc_mirror(word):
     saver=""
     return solution
 
-print(abc_mirror("asd"))
-
-
 
 def create_matrix(word1, word2):
-    new_word = ""
     matrix = []
     index = None
     for i in word2:
         index=letters.index(i)
-        new_word=shift_characters(word1,index)
-        matrix.append(new_word)
+        word1=shift_characters(word1,index)
+        matrix.append(word1)
     return matrix
 
-print(create_matrix("mamas", "papas"))
 
 def zig_zag_concatenate(matrix):
-    word1 = matrix[0]
-    word2 = matrix[1]
-    word3 = matrix[2]
-    word4 = matrix[3]
-    solution = word1[0] + word2[0] + word3[0] + word4[0] + word4[1] + word3[1] + word2[1] + word1[1] + word1[2] + word2[2] + word3[2] + word4[2]
-    return solution
-
-print(zig_zag_concatenate(["aaa", "bbb", "ccc", "ddd"]))
+    solution=""
+    solution1=""
+    solution3=""
+    turn=0
+    while turn!=len(matrix):
+        for i in range(len(matrix)):
+            if turn%2==0:
+                for j in matrix:
+                    solution+=j[turn]
+                solution3 += solution
+                solution = ""
+            else:
+                for j in matrix:
+                    solution1+=j[turn]
+                solution2 = solution1 [::-1]
+                solution3 += solution2
+                solution1 = ""
+            turn+=1
+    return solution3
 
 
 def rotate_right(word, n):
-    word=word.lower()
     letter = [char for char in word]
     letters = [char for char in word]
+    letters2 = [char for char in word]
     for i in range(len(letter)):
         position = 0
-        for j in letters:
+        for j in letters2:
             if letter[i] == j:
                 try:
                     letter[i] = letters[position-n]
                 except IndexError:
-                    letter[i] = letters[(position-n)-len(letters)]
+                    un = n//len(letter)
+                    n = n - un*len(letter)
+                    letter[i] = letters[position-n]
+                letters2[position] = "."
                 break
             else:
                 position += 1
     solution = "".join(letter)
     return solution
 
-print(rotate_right("abcd", 2))
 
 def get_square_index_chars(word):
     solution=""
@@ -127,8 +129,6 @@ def get_square_index_chars(word):
         except:
             continue
     return solution
-
-print(get_square_index_chars("abcdefghijklm"))
 
 
 def remove_odd_blocks(word, block_length):
@@ -150,16 +150,12 @@ def remove_odd_blocks(word, block_length):
     solution.append(block)
     return solution[0::2]
 
-print(remove_odd_blocks("abcdefghijklm", 3))
-
 
 def reduce_to_fixed(word, n):
     word2 = pad_up_to(word, 0, 6)
     n = n//3
     word3 = rotate_right(word2, -n)
     return word3[::-1]
-
-print(reduce_to_fixed("abcdefghijklm", 6))
 
 
 def hash_it(word):
@@ -168,10 +164,17 @@ def hash_it(word):
     'trowdo'
     """
     padded = pad_up_to(word, 15, 19)
+    print(padded)
+    print(abc_mirror(padded))
+    print(create_matrix(padded, abc_mirror(padded)))
     elongated = zig_zag_concatenate(create_matrix(padded, abc_mirror(padded)))
+    print(elongated)
     rotated = rotate_right(elongated, 3000003)
+    print(rotated)
     cherry_picked = get_square_index_chars(rotated)
+    print(cherry_picked)
     halved = remove_odd_blocks(cherry_picked, 3)
+    print(halved)
     key = reduce_to_fixed(halved, 6)
     return key
 
